3a0eaca9161a03c34c2af8e32c5b27c1ddfe6fc6
Ikeda <brandon_ikeda@hotmail.com>
2018-02-17 11:12:03 -0800

    First commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return(
-      <View>
-        <Text>
+      <View style={styles.container}>
+        <Text style={styles.homeText}>
           Hello
         </Text>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1
+  },
+  homeText: {
+    fontSize: 26
+  }
+})
